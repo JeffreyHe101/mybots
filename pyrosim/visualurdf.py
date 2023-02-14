74bfe18ef04457482b109bc5a85a24e812506981
JeffreyHe101 <81978211+JeffreyHe101@users.noreply.github.com>
2023-02-13 18:22:14 -0600

    Add files via upload

--- pyrosim/visualurdf.py
+++ pyrosim/visualurdf.py
@@ -4,13 +4,13 @@ from pyrosim.commonFunctions import Save_Whitespace
 
 class VISUAL_URDF: 
 
-    def __init__(self,origin,geometry):
+    def __init__(self,origin,geometry, str1, str2):
 
         self.origin = origin
 
         self.geometry = geometry 
 
-        self.material = MATERIAL()
+        self.material = MATERIAL(str1, str2)
 
         self.depth = 2
 
